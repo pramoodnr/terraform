832fb136e244c49ba4c1ff005ebec5955cc99b9c
pramoodnr <pramood.nr23@gmail.com>
2026-07-28 12:48:52 -0500

    Create versions.tf

---
+++ environments/dev/versions.tf
@@ -0,0 +1,11 @@
+terraform {
+  required_version = ">= 1.7.0"
+  required_providers {
+    aws = { source = "hashicorp/aws", version = "~> 5.80" }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+  default_tags { tags = local.common_tags }
+}
